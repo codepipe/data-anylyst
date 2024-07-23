0bb72bc16d9082d9ac1fd0faa3dd7132656dd16c
codepipe <automation.hub6@gmail.com>
2024-07-23 19:21:32 +0530

    3 rd commit

--- aaliya.py
+++ aaliya.py
@@ -1,1 +1,7 @@
-hi all welcome to learning exposure of the scm mgmt tool
+hi all welcome
+this is the coding area
+we are data anaylyst
+also a cloud data analyst
+we are pride to collect the company data
+and make them easier to understand the conept of reducint the cost optimization
+and i m the most automation guy to exploit the data into the visulaziation ndash boards:
