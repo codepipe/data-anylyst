ee37ba014fe0b89400b173a89718d1eb7dd9ba0f
codepipe <47555726+codepipe@users.noreply.github.com>
2024-07-23 19:28:47 +0530

    pandas modules added
    yes aaliya written the code

--- aaliya.py
+++ aaliya.py
@@ -1,7 +1,12 @@
-hi all welcome
-this is the coding area
-we are data anaylyst
-also a cloud data analyst
-we are pride to collect the company data
-and make them easier to understand the conept of reducint the cost optimization
-and i m the most automation guy to exploit the data into the visulaziation ndash boards:
+import pandas as pd
+
+# Load the Excel file
+file_path = 'path_to_your_excel_file.xlsx'
+sheet1 = pd.read_excel(file_path, sheet_name='Sheet1')
+sheet2 = pd.read_excel(file_path, sheet_name='Sheet2')
+
+# Perform VLOOKUP-like merge
+result = pd.merge(sheet1, sheet2, on='ID', how='left')
+
+print(result)
+
